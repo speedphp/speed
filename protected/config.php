cc26c9a62eadaec28a419b7ba4d23027dbd47928
zengzhenzhong <zengzhenzhong@yy.com>
2015-08-11 16:26:16 +0800

    增加rewrite信息和数据库配置示例

--- protected/config.php
+++ protected/config.php
@@ -4,37 +4,31 @@ date_default_timezone_set('PRC');
 
 
 $config = array(
-//	'rewrite' => array(
-//		'admin/news.html' => 'admin/main/two',
-//		'<m>/<c>/<a>' => '<m>/<c>/<a>',
-//		'/' => 'new/main/index',
-//	),
+	'rewrite' => array(
+		'admin/index.html' => 'admin/main/index',
+		'admin/<c>_<a>.html'    => 'admin/<c>/<a>', 
+		'<c>/<a>'          => '<c>/<a>',
+		'/'                => 'main/index',
+	),
 );
 
 $domain = array(
-	"localhost" => array(
+	"localhost" => array( // 调试配置
 		'debug' => 1,
 		'mysql' => array(
-			"read" => array(
-				'MYSQL_HOST' => 'localhost',
-				'MYSQL_PORT' => '3306',
-				'MYSQL_USER' => 'root',
-				'MYSQL_DB'   => 'test',
-				'MYSQL_PASS' => '',
-				'MYSQL_CHARSET' => 'utf8',
-			),
-			"write" => array(
-				'MYSQL_HOST' => 'localhost',
-				'MYSQL_PORT' => '3306',
-				'MYSQL_USER' => 'root',
-				'MYSQL_DB'   => 'test',
-				'MYSQL_PASS' => '',
-				'MYSQL_CHARSET' => 'utf8',
-			),
-		),
-		
-		
 
+				'MYSQL_HOST' => 'localhost',
+				'MYSQL_PORT' => '3306',
+				'MYSQL_USER' => 'root',
+				'MYSQL_DB'   => 'test',
+				'MYSQL_PASS' => '',
+				'MYSQL_CHARSET' => 'utf8',
+
+		),
+	),
+	"speedphp.com" => array( //线上配置
+		'debug' => 0,
+		'mysql' => array(),
 	),
 );
 
