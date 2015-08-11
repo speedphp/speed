f5b7651598bebea93762e72ff17769b3d01cc2fb
zengzhenzhong <zengzhenzhong@yy.com>
2015-08-11 11:00:12 +0800

    增加modules的模块（子应用）示例

---
+++ protected/controller/admin/LoginController.php
@@ -0,0 +1,11 @@
+<?php
+class LoginController extends BaseController
+{
+	function actionIndex(){
+		echo "hello login!";
+	}
+
+	function actionTest(){
+		echo "hello login test";
+	}
+}
---
+++ protected/controller/admin/MainController.php
@@ -0,0 +1,11 @@
+<?php
+class MainController extends BaseController
+{
+	function actionIndex(){
+		echo "hello admin!";
+	}
+
+	function actionTest(){
+		echo "hello admin test";
+	}
+}
