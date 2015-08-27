e721a55ef629fddf278a3eeb72b376f3dcb2e24a
jake <webmaster@speedphp.com>
2015-08-27 12:14:59 +0800

    修正一个分页对总数判断无法实现的问题

--- protected/lib/speed.php
+++ protected/lib/speed.php
@@ -181,6 +181,7 @@ class Model{
 
 		$sql = ' FROM '.$this->table_name.$conditions["_where"];
 		if(is_array($limit)){
+			$total = $this->query('SELECT COUNT(*) as M_COUNTER '.$sql, $conditions["_bindParams"]);
 			$limit = $limit + array(1, 10, 10);
 			$limit = $this->pager($limit[0], $limit[1], $limit[2], $total[0]['M_COUNTER']);
 			$limit = empty($limit) ? '' : ' LIMIT '.$limit['offset'].','.$limit['limit'];			
