bc898dead0dc6700a9956005973abd252e688eac
jake <jake@speedphp.com>
2016-03-29 15:29:21 +0800

    为了避免开始使用时会不正确配置域名导致程序错误，config.php加入判断

--- protected/config.php
+++ protected/config.php
@@ -31,5 +31,7 @@ $domain = array(
 		'mysql' => array(),
 	),
 );
+// 为了避免开始使用时会不正确配置域名导致程序错误，加入判断
+if(empty($domain[$_SERVER["HTTP_HOST"]])) die("配置域名不正确，请确认".$_SERVER["HTTP_HOST"]."的配置是否存在！");
 
 return $domain[$_SERVER["HTTP_HOST"]] + $config;
